7be554e326f7c542c4ff560a562f6a1a48cce896
yuna <yuna744@gmail.com>
2023-12-26 11:54:14 +0900

    1226 gcTime 메모리 제거

--- src/hooks/useUsers.js
+++ src/hooks/useUsers.js
@@ -25,9 +25,15 @@ export const useUserQuery = () => {
     queryFn: fetchUser,
     refetchOnWindowFocus: false, // 다른 브라우저 탭으로 이동했다가 다시 넘어오면 재패칭 안함
     refetchOnMount: false, //해당 컴포넌트 재 마운트시 reftetching 안함
-    catchTime: 1000 * 10,
+    gcTime: 1000 * 10,
   });
 };
+/*
+fresh : 서버 데이터를 최신상태로 인식하는 상태
+stale : 서버 데이터를 오래된 데이터로 인식하는 상태
+inactive : 서버 데이터가 더 이상 해당 컴포넌트에서 활용되지 않는 상태
+gcTime : inactive가 되자마자 gcTime에 등록된 시간 값이 소진되면서 해당 시간이 지나면 가비지 컬렌터가 해당 메모리 제거
+*/
 /*
 react-query 개발툴에서 확인할 수 있는 상태값 5가지 정리
 fresh : 비동기 데이터가 현재 최산상태 (refetching할 필요가 없는 신선한 상태)
