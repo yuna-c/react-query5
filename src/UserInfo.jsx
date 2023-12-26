0f4941b354382ef292f9685581448c3980a2a12a
yuna <yuna744@gmail.com>
2023-12-26 10:03:20 +0900

    1226 react-query및 전용 개발자 도구 세팅

---
+++ src/UserInfo.jsx
@@ -0,0 +1,7 @@
+export default function UserInfo() {
+  // 비동기 데이터 관리 해보기
+  const fetchUser = async () => {
+    const response = await fetch();
+  };
+  return <div className="UserInfo">UserInfo</div>;
+}
