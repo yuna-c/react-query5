794cec3b78f5ff4c335b0e13a406b74ce4662049
yuna <yuna744@gmail.com>
2023-12-26 10:00:57 +0900

    1226 react-query및 전용 개발자 도구 세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 function App() {
-  return <div className="App"></div>;
+  //생성자 객체(비동기 데이터)
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
